2d1add787c1fee9288189d45b67308957af5b841
Youssef Aouadni <aouadniyoussef01@gmail.com>
2025-05-24 16:00:39 +0100

    update

--- test/live_barcode_scanner_test.dart
+++ test/live_barcode_scanner_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/services.dart';
-import 'package:live_barcode_scanner/live_barcode_scanner_method_channel.dart';
+import 'package:ml_kit_live_barcode_scanner/live_barcode_scanner_method_channel.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('live_barcode_scanner');
